a62ba4307968e1fc389f58599cb91c82a18f1523
linux <luyiqi.lili@gmail.com>
2025-08-30 12:31:10 +0800

    update

---
+++ src/lib/util.ts
@@ -0,0 +1,12 @@
+/*
+liveConfig.ts
+*/
+
+export function escapeHtml(text: string): string {
+    return text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;");
+}
