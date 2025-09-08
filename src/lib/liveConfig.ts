24aa5d6b35a3989ad9cad50ea53afc5fee12cbd4
linux <luyiqi.lili@gmail.com>
2025-09-09 01:05:21 +0800

    启用生产

--- src/lib/liveConfig.ts
+++ src/lib/liveConfig.ts
@@ -511,7 +511,7 @@ export const backupConfig: BackupMapping[] = [
     ]
   },
 
-  /*
+  
   //紫罗大群
   //匹配区
   {
@@ -835,5 +835,5 @@ export const backupConfig: BackupMapping[] = [
       { chat_id: -1003066803437, threadId: 14 }
     ]
   },
-*/
+
 ];
