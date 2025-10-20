da8011218f35ec50807328940cbe7a4113429ea0
linux <luyiqi.lili@gmail.com>
2025-10-20 19:27:36 +0800

    update coin list uid

--- src/commands/coin.ts
+++ src/commands/coin.ts
@@ -709,7 +709,7 @@ export async function handleCoin(parsedMessage: ParsedUpdate, env: CoinEnv): Pro
       for (const [idx, [uid, bal]] of top.entries()) {
         if (!isNaN(Number(uid))) {
           const member = await TgMessage.fetchChatMember(env, chatId, Number(uid));
-          textLines.push(`${member.first_name}:${bal}💰`);
+          textLines.push(`${member.first_name} ${uid}:${bal}💰`);
         }
       }
       const out = `🏆 财富榜\n  <blockquote expandable>` + textLines.join("\n") + ` </blockquote>`;
